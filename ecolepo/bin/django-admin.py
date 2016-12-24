096c0f38cbb71aa98696ba3ecd62723d73738d18
mcueto <cueto@live.cl>
2016-12-24 14:34:44 -0300

    Added some packages

---
+++ ecolepo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mcueto/development/FREE_SOFTWARE_COLABORATIONS/PymeLibre/python-virtual-environments/ecolepo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
